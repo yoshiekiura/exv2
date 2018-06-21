93c0237c70f6871001d772cc205ad31a80809466
MohamedZainFayed <mohamed.z.fayed@gmail.com>
2018-06-21 22:17:22 +0200

    Upgrading to Rails 4.0.13

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 4.0.12'
+gem 'rails', '~> 4.0.13'
 gem 'rails-i18n'
+gem 'rake', '< 11.0'
 
 gem 'mysql2', '~> 0.3.21'
 gem 'daemons-rails'
@@ -70,7 +71,7 @@ gem 'simple_captcha2', require: 'simple_captcha'
 gem 'rest-client', '~> 1.6.8'
 
 group :development, :test do
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
   gem 'faker', '~> 1.4.3'
   gem 'mina'
   gem 'mina-slack', github: 'InfraexDev/mina-slack'
@@ -87,13 +88,13 @@ group :development, :test do
 end
 
 group :test do
-  gem 'database_cleaner'
-  gem 'mocha', :require => false
-  gem 'shoulda-matchers'
   gem 'capybara'
+  gem 'database_cleaner'
   gem 'launchy'
-  gem 'selenium-webdriver'
+  gem 'mocha', :require => false
   gem 'poltergeist'
+  gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 
   # rspec-rails rely on test-unit if rails version less then 4.1.0
   # but test-unit has been removed from ruby core since 2.2.0
--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -1,91 +1,63 @@
-feature 'show account info', js: true do
-  let!(:member) { create :member, :verified_identity }
+describe Market do
+  context 'visible market' do
+    # it { expect(Market.orig_all.count).to eq(2) }
+    it { expect(Market.all.count).to eq(1) }
+  end
 
-  let!(:bid_account) do
-    member.get_account(:usd).tap do |a|
-      a.plus_funds 1000
-      a.save!
+  context 'markets hash' do
+    it 'should list all markets info' do
+      expect(Market.to_hash).to eq ({ btccny: { name: 'BTC/CNY', base_unit: 'btc', quote_unit: 'cny' } })
     end
   end
 
-  let!(:ask_account) do
-    member.get_account(:btc).tap do |a|
-      a.plus_funds 2000
-      a.save!
+  context 'market attributes' do
+    let(:log) { Market.find('btccny') }
+
+    it 'id' do
+      expect(log.id).to eq 'btccny'
+    end
+
+    it 'name' do
+      expect(log.name).to eq 'BTC/CNY'
+    end
+
+    it 'base_unit' do
+      expect(log.base_unit).to eq 'btc'
+    end
+
+    it 'quote_unit' do
+      expect(log.quote_unit).to eq 'cny'
+    end
+
+    it 'visible' do
+      expect(log.visible).to be true
     end
   end
 
-  let!(:ask_order) { create :order_ask, price: '23.6', member: member }
-  let!(:bid_order) { create :order_bid, price: '21.3', member: member }
-  let!(:ask_name)  { 'BTC' }
+  context 'enumerize' do
+    subject { Market.enumerize }
 
-  let(:global) { Global[:btcusd] }
-
-  scenario 'user can place a buy order by filling in the order form' do
-    sign_in member
-    click_on I18n.t('header.market')
-
-    page.within_window(windows.last) do
-      expect do
-        fill_in 'order_bid_price', with: 22.2
-        fill_in 'order_bid_origin_volume', with: 45
-        expect(page.find('#order_bid_total').value).to be_d (45 * 22.2).to_d
-
-        click_button I18n.t('private.markets.bid_entry.action', currency: ask_name)
-        sleep 0.1 # sucks :(
-        expect(page.find('#bid_entry span.label-success').text).to eq I18n.t('private.markets.show.success')
-      end.to change { OrderBid.all.count }.by(1)
-    end
+    it { is_expected.to be_has_key :btccny }
+    it { is_expected.to be_has_key :ptsbtc }
   end
 
-  scenario 'user can place a sell order by filling in the order form' do
-    sign_in member
-    click_on I18n.t('header.market')
+  context 'shortcut of global access' do
+    let(:log) { Market.find('btccny') }
 
-    page.within_window(windows.last) do
-      expect do
-        fill_in 'order_ask_price', with: 22.2
-        fill_in 'order_ask_origin_volume', with: 45
-        expect(page.find('#order_ask_total').value).to be_d (45 * 22.2).to_d
-
-        click_button I18n.t('private.markets.ask_entry.action', currency: ask_name)
-        sleep 0.1 # sucks :(
-        expect(page.find('#ask_entry span.label-success').text).to eq I18n.t('private.markets.show.success')
-      end.to change { OrderAsk.all.count }.by(1)
+    it 'bids' do
+      expect(log.bids).to be
     end
-  end
 
-  scenario 'user can fill order form by clicking on an existing orders in the order book' do
-    global.stubs(:asks).returns([[ask_order.price, ask_order.volume]])
-    global.stubs(:bids).returns([[bid_order.price, bid_order.volume]])
-    Global.stubs(:[]).returns(global)
-
-    sign_in member
-    click_on I18n.t('header.market')
-
-    page.within_window(windows.last) do
-      page.find('.asks tr[data-order="0"]').click
-      expect(find('#order_bid_price').value).to be_d ask_order.price
-      expect(find('#order_bid_origin_volume').value).to be_d ask_order.volume
-      expect(find('#order_ask_price').value).to be_d ask_order.price
-      expect(find('#order_ask_origin_volume').value).to be_d ask_order.volume
-
-      page.find('.bids tr[data-order="0"]').click
-      expect(find('#order_ask_price').value).to be_d bid_order.price
-      expect(find('#order_ask_origin_volume').value).to be_d bid_order.volume
-      expect(find('#order_bid_price').value).to be_d bid_order.price
-      expect(find('#order_bid_origin_volume').value).to be_d bid_order.volume
+    it 'asks' do
+      expect(log.asks).to be
     end
-  end
 
-  scenario 'user can view his account balance' do
-    sign_in member
-    click_on I18n.t('header.market')
+    it 'trades' do
+      expect(log.trades).to be
+    end
 
-    page.within_window(windows.last) do
-      # account balance at place order panel
-      expect(page.find('#bid_entry .current-balance').text).to be_d bid_account.balance
-      expect(page.find('#ask_entry .current-balance').text).to be_d ask_account.balance
+    it 'ticker' do
+      expect(log.ticker).to be
     end
   end
 end
